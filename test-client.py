f2cfc97d6905b6d9106ec247da9b83680fed223f
Краснобаев Евгений <1@krasnobaev.com>
2023-07-28 23:52:53 +0500

    Впилил общение по JSON между клиент сервером

--- test-client.py
+++ test-client.py
@@ -1,12 +1,13 @@
-import socket
+import socket, json
 
 def test_connection():
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.settimeout(1)
     s.connect(("localhost", 2020))
-    s.sendall(b"test")
+    data = {'request':'database'}
+    s.sendall(json.dumps(data).encode())
     print(s.recv(5000))
     s.close()
 
-for i in range(10004): 
+for i in range(1004): 
     test_connection()
--- usecase/Server.cpp
+++ usecase/Server.cpp
@@ -1,5 +1,7 @@
 #include "Server.h"
 #include <QDebug>
+#include <QJsonDocument>
+#include <QJsonObject>
 
 Server::Server(IDatabase* db, QObject* parent)
     : QObject { parent }
@@ -12,6 +14,30 @@ void Server::processClientRequest(QByteArray data)
     IClient* client = qobject_cast<IClient*>(sender());
     if (client == nullptr)
         return;
+
+    // итак, основной обмен данными
+    QString requestBody = QString::fromUtf8(data);
+
+    QJsonParseError error;
+    QJsonDocument requestJson = QJsonDocument::fromJson(data, &error);
+    if (requestJson.isNull()) {
+        client->reply(QString("Error %1").arg(error.errorString()).toUtf8());
+        return;
+    }
+
+    if (!requestJson.isObject()) {
+        client->reply("Request is not object");
+        return;
+    }
+
+    QJsonObject request = requestJson.object();
+    // например наш запрос такой {"request"="database"}
+    if (request.contains("request") && request["request"].toString().compare("database", Qt::CaseInsensitive) == 0) {
+        QJsonObject answer;
+        answer["o1"] = "o2";
+        client->reply(QJsonDocument { answer }.toJson());
+        return;
+    }
 
     qDebug() << data;
     client->reply("OK");
